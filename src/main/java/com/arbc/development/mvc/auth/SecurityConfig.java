b699e293477c15f485c77b09438962fcc4b9b863
Adrián Bedón <arbedon2002@gmail.com>
2023-11-12 22:40:41 -0500

    Validación de Roles

--- src/main/java/com/arbc/development/mvc/auth/SecurityConfig.java
+++ src/main/java/com/arbc/development/mvc/auth/SecurityConfig.java
@@ -36,6 +36,10 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
             .requestMatchers(HttpMethod.GET, "/users").permitAll()
+            .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.PUT, "/users/{id}").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and()
             .addFilter(new JwtAuthFilter(authenticationConfiguration.getAuthenticationManager()))
