e0e9e6fd0078bed1fcd650146fd392aafbb0292c
Adrián Bedón <arbedon2002@gmail.com>
2023-11-16 11:54:14 -0500

    Cambios de permisos

--- src/main/java/com/arbc/development/mvc/auth/SecurityConfig.java
+++ src/main/java/com/arbc/development/mvc/auth/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
