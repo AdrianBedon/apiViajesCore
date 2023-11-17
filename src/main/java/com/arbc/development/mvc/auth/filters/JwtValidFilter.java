28af6eed2f0b1971588cc0bf9a07e5c29e26ad36
Adrián Bedón <arbedon2002@gmail.com>
2023-11-17 16:37:28 -0500

    Cambios en el status de error

--- src/main/java/com/arbc/development/mvc/auth/filters/JwtValidFilter.java
+++ src/main/java/com/arbc/development/mvc/auth/filters/JwtValidFilter.java
@@ -72,7 +72,7 @@ public class JwtValidFilter extends BasicAuthenticationFilter {
             body.put("error", e.getMessage());
             body.put("message", "El token no es válido!");
             response.getWriter().write(new ObjectMapper().writeValueAsString(body));
-            response.setStatus(403);
+            response.setStatus(401);
             response.setContentType("application/json");
         }
     }
