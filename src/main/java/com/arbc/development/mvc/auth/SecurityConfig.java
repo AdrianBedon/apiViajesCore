32d6325cdd1f23c7b3963c5cd0eab76feeed86f3
Adrián Bedón <arbedon2002@gmail.com>
2024-01-06 10:13:02 -0500

    Public

--- src/main/java/com/arbc/development/mvc/auth/SecurityConfig.java
+++ src/main/java/com/arbc/development/mvc/auth/SecurityConfig.java
@@ -43,10 +43,10 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
-            .requestMatchers(HttpMethod.GET, "/users", "/hotels", "/hotels/{city}", "/cities", "/flights", "/flights/{city}", "/packages").permitAll()
+            .requestMatchers(HttpMethod.GET, "/users", "/hotels", "/hotels/{city}", "/cities", "/flights", "/flights/{city}", "/packages", "/peak/reportSale").permitAll()
             .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("USER", "ADMIN")
             .requestMatchers(HttpMethod.POST, "/packages").hasAnyRole("USER","ADMIN")
-            .requestMatchers(HttpMethod.GET, "/peak", "/off", "/peak/reportSale").hasRole("ADMIN")
+            .requestMatchers(HttpMethod.GET, "/peak", "/off").hasRole("ADMIN")
             .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
             .requestMatchers(HttpMethod.DELETE, "/packages/{id}").hasAnyRole("USER", "ADMIN")
             .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasRole("ADMIN")
